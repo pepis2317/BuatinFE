c09d6f770431550a2c9389cedac3df3d1541a78f
Vino Arsena <vinoarsena@Arsenas-MacBook.local>
2025-10-29 11:04:54 +0700

    Update: installed gluestack

---
+++ gluestack-ui.config.ts
@@ -0,0 +1,39 @@
+import { createConfig } from "@gluestack-ui/themed";
+
+export const config = createConfig({
+  tokens: {
+    colors: {
+      primary: "#348AFA",
+      secondary: "#5CCFA3",
+      backgroundLight: "#FFFFFF",
+      backgroundDark: "#222831",
+      textLight: "#31363F",
+      error: "#EF4444",
+    },
+    radii: { sm: 6, md: 12, lg: 20 },
+    spacing: { sm: 8, md: 16, lg: 24 },
+  },
+  components: {
+    Button: {
+      variants: {
+        solid: {
+          bg: "$primary",
+          _text: { color: "$white" },
+        },
+      },
+      baseStyle: {
+        borderRadius: "$md",
+      },
+    },
+    Input: {
+      baseStyle: {
+        borderRadius: "$md",
+      },
+    },
+    Alert: {
+      baseStyle: {
+        borderRadius: "$sm",
+      },
+    },
+  },
+} as any);

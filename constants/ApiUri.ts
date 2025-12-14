1c019399dd0db3e56349ef50420e7d4c116e8506
pepis2317 <candradenis2317@gmail.com>
2025-12-14 16:16:51 +0700

    removed ios and android folders (dw emg g perlu)

--- constants/ApiUri.ts
+++ constants/ApiUri.ts
@@ -1,4 +1,4 @@
 
-export const API_URL = "http://localhost:5026/api/v1";
+export const API_URL = "https://18a39121c0ac.ngrok-free.app/api/v1";
 //export const API_URL = "http://172.20.10.3:5026/api/v1";
 //export const API_URL = "https://10987a367973.ngrok-free.app/api/v1"
